8b6e2b5f3f1ef7e7fb05c82fd5f98b02d6874d53
remmy.benito <remmy.benito@gmail.com>
2019-11-30 20:03:00 -0600

    Add project files.

---
+++ IMDdWebScraper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IMDdWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Create Object From Hello Scrape class
+            IMDdWebScraper.Sample.HelloScraper scrape = new IMDdWebScraper.Sample.HelloScraper();
+            // Start Scraping
+            scrape.Start();
+        }
+    }
+    }
+}
---
+++ IMDdWebScraper/Sample/HelloScraper.cs
@@ -0,0 +1,48 @@
+﻿using IronWebScraper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMDdWebScraper.Sample
+{
+    public class HelloScraper : WebScraper
+    {
+        /// <summary>
+        /// Override this method initialize your web-scraper.
+        /// Important tasks will be to Request at least one start url... and set allowed/banned domain or url patterns.
+        /// </summary>
+        public override void Init()
+        {
+            License.LicenseKey = "LicenseKey"; // Write License Key
+            this.LoggingLevel = WebScraper.LogLevel.All; // All Events Are Logged
+            this.Request("https://blog.scrapinghub.com", Parse);
+        }
+
+        /// <summary>
+        /// Override this method to create the default Response handler for your web scraper.
+        /// If you have multiple page types, you can add additional similar methods.
+        /// </summary>
+        /// <param name="response">The http Response object to parse</param>
+        public override void Parse(Response response)
+        {
+            // set working directory for the project
+            this.WorkingDirectory = AppSetting.GetAppRoot() + @"\HelloScraperSample\Output\";
+            // Loop on all Links
+            foreach (var title_link in response.Css("h2.entry-title a"))
+            {
+                // Read Link Text
+                string strTitle = title_link.TextContentClean;
+                // Save Result to File
+                Scrape(new ScrapedData() { { "Title", strTitle } }, "HelloScraper.Jsonl");
+            }
+            // Loop On All Links
+            if (response.CssExists("div.prev-post > a[href]"))
+            {
+                // Get Link URL
+                var next_page = response.Css("div.prev-post > a[href]")[0].Attributes["href"];
+                // Scrpae Next URL
+                this.Request(next_page, Parse);
+            }
+        }
+    }
+}
